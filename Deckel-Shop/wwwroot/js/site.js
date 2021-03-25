9f4c6c5c37fe6a37d85327457514261702ca05f5
Jenny Wong <jennywong567@gmail.com>
2021-03-25 08:11:18 +0100

    added function to showpassword in edit customer (only works on ONE place)

--- Deckel-Shop/wwwroot/js/site.js
+++ Deckel-Shop/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Admin -> Edit customer -> Show password
+//$(document).ready(function () {
+//    $('.showPassword').on('change', function () {
+//        var isChecked = $(this).prop('checked');
+//        if (isChecked) {
+//            $('.my-password').attr('type', 'text');
+//        } else {
+//            $('.my-password').attr('type', 'Password');
+//        }
+//    });
+//});
+
+
+function showPassword() {
+    var x = document.getElementById("inputPassword");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+};
