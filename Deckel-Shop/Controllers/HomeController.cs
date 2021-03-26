4f83134a07e67fdf7ad9fdc6629db7b19fc5c964
DESKTOP-FIM5DOL\fisni <fisnik.haliti@hotmail.com>
2021-03-26 14:22:05 +0100

    Career page

--- Deckel-Shop/Controllers/HomeController.cs
+++ Deckel-Shop/Controllers/HomeController.cs
@@ -43,5 +43,10 @@ namespace Deckel_Shop.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Career()
+        {
+            return View();
+        }
     }
 }
