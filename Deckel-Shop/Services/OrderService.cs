87d93e6f463de5295a74b32f0518302b2d74ca1c
yahya allouhaibi <yahyallouhaibi@outlook.com>
2021-04-01 12:16:57 +0200

    worked on services for the admin page

---
+++ Deckel-Shop/Services/OrderService.cs
@@ -0,0 +1,61 @@
+﻿using Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Deckel_Shop.Services
+{
+    public class OrderService
+    {
+        private readonly DeckelShopContext deckelShopContext;
+
+        public OrderService()
+        {
+            deckelShopContext = new DeckelShopContext();
+        }
+
+        // info - delete - sendOrder
+
+
+        public IEnumerable<Order> GetAllOrdersByOrderStatus(string status)
+        {
+            if (status != null) { 
+            return deckelShopContext.Orders.Where(o=> o.OrderStatus == status).AsEnumerable();
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEnumerable<Order> GetOrder(int id)
+        {
+            return deckelShopContext.Orders.Where(o => o.OrderId == id).AsEnumerable();
+        }
+
+        public void AddOrder(Order order)
+        {
+            deckelShopContext.Orders.Add(order);
+            deckelShopContext.SaveChanges();
+        }
+
+        public void RemoveOrder(Order order)
+        {
+            deckelShopContext.Orders.Remove(order);
+            deckelShopContext.SaveChanges();
+        }
+
+        public void SendOrder (Order order)
+        {
+            var currentOrder = deckelShopContext.Orders.FirstOrDefault(x => x.OrderId == order.OrderId);
+            if (currentOrder != null)
+            {
+                //change orderstatus to int???
+                currentOrder.OrderStatus = "Shipped";
+            }
+            deckelShopContext.SaveChanges();
+        }
+
+    }
+}
---
+++ Deckel-Shop/Services/StockService.cs
@@ -0,0 +1,50 @@
+﻿using Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Deckel_Shop.Services
+{
+    public class StockService
+    {
+        private readonly DeckelShopContext deckelShopContext;
+        public StockService()
+        {
+            deckelShopContext = new DeckelShopContext();
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return deckelShopContext.Products.AsEnumerable();
+        }
+        public IEnumerable<Product> GetProduct(int id)
+        {
+            return deckelShopContext.Products.Where(p => p.Id == id).AsEnumerable();
+        }
+
+        public void AddProduct(Product product)
+        {
+            deckelShopContext.Products.Add(product);
+            deckelShopContext.SaveChanges(); 
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            deckelShopContext.Products.Remove(product);
+            deckelShopContext.SaveChanges();
+        }
+
+        public void EditProduct(Product product)
+        {
+            var currentProduct = deckelShopContext.Products.FirstOrDefault(x => x.Id == product.Id);
+            if (currentProduct != null)
+            {
+                currentProduct = product;
+            }
+            deckelShopContext.SaveChanges();
+        }
+
+
+    }
+}
