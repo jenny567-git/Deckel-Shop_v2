d8e1d77f534d2d65bc600887a0d1573e29020c69
Andreas Linné <andreas_linne@hotmail.com>
2021-04-01 13:34:48 +0200

    Added js for modals. Added test api

---
+++ Deckel-Shop/ApiControllers/OrdersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Deckel_Shop.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        public Test Get()
+        {
+            Test t = new Test();
+            t.Id = 3434;
+            return t;
+        }
+
+
+
+        
+        [HttpPost]
+        public Test Post(Test id)
+        {
+            id.Id = 23123;
+            return id;
+        }
+    }
+
+    public class Test
+    {
+        public int Id { get; set; }
+    }
+}
