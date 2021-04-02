94467422b9f31714946bbc6fa137b128c0e30f2d
Jenny Wong <jennywong567@gmail.com>
2021-04-02 09:51:22 +0200

    product view linked to database

--- Deckel-Shop/Controllers/ProductController.cs
+++ Deckel-Shop/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Deckel_Shop.Models;
+using Deckel_Shop.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,26 +10,16 @@ namespace Deckel_Shop.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly StockService _ss;
+
+        public ProductController()
+        {
+            _ss = new StockService();
+        }
         public IActionResult Index()
         {
-            List<Cap> caplist = new List<Cap>()
-            {
-            new Cap() { Id = 1, ProductName = "pName", Price = 12, Stock = 20 },
-            new Cap() { Id = 2, ProductName = "pName2", Price = 2, Stock = 30 },
-            new Cap() { Id = 2, ProductName = "pName2", Price = 2, Stock = 30 },
-            new Cap() { Id = 2, ProductName = "pName2", Price = 2, Stock = 30 },
-            new Cap() { Id = 2, ProductName = "pName2", Price = 2, Stock = 30 },
-            new Cap() { Id = 2, ProductName = "pName2", Price = 2, Stock = 30 },
-            new Cap() { Id = 2, ProductName = "pName2", Price = 2, Stock = 30 },
-            new Cap() { Id = 2, ProductName = "pName2", Price = 2, Stock = 30 },
-            new Cap() { Id = 2, ProductName = "pName2", Price = 2, Stock = 30 },
-            new Cap() { Id = 2, ProductName = "pName2", Price = 2, Stock = 30 },
-            new Cap() { Id = 2, ProductName = "pName2", Price = 2, Stock = 30 },
-            new Cap() { Id = 2, ProductName = "pName2", Price = 2, Stock = 30 },
-            new Cap() { Id = 3, ProductName = "pName3", Price = 22, Stock = 40 }
-
-            };
-            return View(caplist);
+            var listOfProducts = _ss.GetAllProducts();
+            return View(listOfProducts);
         }
     }
 }
