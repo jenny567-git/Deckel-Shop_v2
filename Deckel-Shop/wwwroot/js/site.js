11947f83ee1007e40cb6dc5224f3b20b295d0579
Alexander Hammargren <73111694+Almightyfox@users.noreply.github.com>
2021-04-05 13:11:43 +0200

    Made a new nawbar with flex functionality

--- Deckel-Shop/wwwroot/js/site.js
+++ Deckel-Shop/wwwroot/js/site.js
@@ -3,6 +3,36 @@
 
 // Write your JavaScript code.
 
+/***NavBar Function***/
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        //toggle nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        //Burger animation
+        burger.classList.toggle('toggle');
+
+    });
+
+
+}
+
+navSlide();
+
+
+
 var slideIndex = 1;
 $(document).ready(function () {
 
