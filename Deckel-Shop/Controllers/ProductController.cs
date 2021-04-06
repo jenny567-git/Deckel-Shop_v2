de1a617b0b45b6b00afd480a0cb5cdcb5fe681d6
Jenny Wong <jennywong567@gmail.com>
2021-04-06 09:35:01 +0200

    added sort function to shop view

--- Deckel-Shop/Controllers/ProductController.cs
+++ Deckel-Shop/Controllers/ProductController.cs
@@ -17,12 +17,72 @@ namespace Deckel_Shop.Controllers
         {
             _ss = new StockService();
         }
-        public IActionResult Index()
+
+        //public IActionResult Index(string sortOrder = "name")
+        //{
+
+        //    var listOfProducts = _ss.GetAllAvailableProducts();
+        //    listOfProducts = sortOrder switch
+        //    {
+        //        "name_desc" => listOfProducts.OrderByDescending(p => p.Name),
+        //        "price_desc" => listOfProducts.OrderByDescending(p => p.Price),
+        //        "price_asc" => listOfProducts.OrderBy(p => p.Price),
+        //        _ => listOfProducts.OrderBy(p => p.Name),
+        //    };
+        //    return View(listOfProducts);
+        //}
+
+        //public IActionResult Index(string sortOrder)
+        //{
+
+        //    var listOfProducts = _ss.GetAllAvailableProducts();
+        //    switch (sortOrder)
+        //    {
+        //        case "name_desc":
+        //            listOfProducts = listOfProducts.OrderByDescending(p => p.Name);
+        //            break;
+        //        case "price_desc":
+        //            listOfProducts = listOfProducts.OrderByDescending(p => p.Price);
+        //            break;
+        //        case "price_asc":
+        //            listOfProducts = listOfProducts.OrderBy(p => p.Price);
+        //            break;
+        //        default:
+        //            listOfProducts = listOfProducts.OrderBy(p => p.Name);
+        //            break;
+        //    }
+        //    return View(listOfProducts);
+        //}
+        
+        public IActionResult Index(string sortOrder)
         {
+
             var listOfProducts = _ss.GetAllAvailableProducts();
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    listOfProducts = listOfProducts.OrderByDescending(p => p.Name);
+                    break;
+                case "price_desc":
+                    listOfProducts = listOfProducts.OrderByDescending(p => p.Price);
+                    break;
+                case "price_asc":
+                    listOfProducts = listOfProducts.OrderBy(p => p.Price);
+                    break;
+                default:
+                    listOfProducts = listOfProducts.OrderBy(p => p.Name);
+                    break;
+            }
             return View(listOfProducts);
         }
-       
-        
+
+        //public  IActionResult Index()
+        //{
+
+        //    var listOfProducts = _ss.GetAllAvailableProducts();
+
+        //    return View(listOfProducts);
+        //}
+
     }
 }
