8b658d6f252b4bd019aae7104bd9579413cc95af
Andreas Linné <andreas_linne@hotmail.com>
2021-04-06 17:33:03 +0200

    test

--- Deckel-Shop/wwwroot/js/site.js
+++ Deckel-Shop/wwwroot/js/site.js
@@ -16,7 +16,7 @@ const navSlide = () => {
         //Animate Links
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
-                link.style.animation = '';
+                //link.style.animation = '';
             } else {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
             }
