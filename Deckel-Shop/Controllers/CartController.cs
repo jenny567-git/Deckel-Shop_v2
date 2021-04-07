65f29bc50cbf4f6b9de9eba3ec548f330cd3e02d
DESKTOP-FIM5DOL\fisni <fisnik.haliti@hotmail.com>
2021-04-07 19:33:03 +0200

    lite ändringar på checkout

--- Deckel-Shop/Controllers/CartController.cs
+++ Deckel-Shop/Controllers/CartController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Database.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,9 +17,74 @@ namespace Deckel_Shop.Controllers
             return View();
         }
 
+        private SqlConnection con;
+
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["getconn"].ToString();
+            con = new SqlConnection(constr);
+
+        }
+
+        [HttpPost]
+        public ActionResult AddOrder(Customer detail, OrderedItem order)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (AddOrderBool(detail, order))
+                    {
+                        ViewBag.Message = "Order added successfully";
+                    }
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public bool AddOrderBool(Customer obj, OrderedItem obj1)
+        {
+
+            connection();
+            SqlCommand com = new SqlCommand("AddOrderDetail", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@FirstName", obj.FirstName);
+            com.Parameters.AddWithValue("@LastName", obj.LastName);
+            com.Parameters.AddWithValue("@Email", obj.Email);
+            com.Parameters.AddWithValue("@Country", obj.Country);
+            com.Parameters.AddWithValue("@City", obj.City);
+            com.Parameters.AddWithValue("@Address", obj.Street);
+            com.Parameters.AddWithValue("@ZipCode", obj.ZipCode);
+            com.Parameters.AddWithValue("@OrderId", obj1.OrderId);
+            com.Parameters.AddWithValue("@ProductId", obj1.ProductId);
+            com.Parameters.AddWithValue("@Amount", obj1.Amount);
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+            }
+        }
         public IActionResult Checkout()
         {
             return View();
         }
+
     }
+
+    
+    
 }
