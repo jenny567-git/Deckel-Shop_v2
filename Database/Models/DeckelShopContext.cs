e9e1bca34eb6756e5aecdaa9c6459595f5fbcf3b
Sebastian Larsson <seblarsson87@gmail.com>
2021-04-09 14:52:21 +0200

    Added new connection string

--- Database/Models/DeckelShopContext.cs
+++ Database/Models/DeckelShopContext.cs
@@ -27,7 +27,7 @@ namespace Database.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Database=DeckelShop;Server=.;Integrated Security=SSPI");
+                optionsBuilder.UseSqlServer("Server=tcp:ourfantasticwebshopdatabaseserver.database.windows.net,1433;Initial Catalog=group1-db2;Persist Security Info=False;User ID=superuser;Password=SuperMario1337;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
